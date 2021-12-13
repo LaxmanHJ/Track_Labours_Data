63c11192f758f63342f9b69424726d612516f41c
LaxmanHJ <laxmanhjeergal@gmail.com>
2021-12-14 03:21:58 +0530

    custom lookup Searchquerying account,  ui in lwc

--- force-app/main/default/lwc/labourDataSearch/labourDataSearch.js
+++ force-app/main/default/lwc/labourDataSearch/labourDataSearch.js
@@ -1,3 +1,77 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import lookupSearchController from '@salesforce/apex/inputLookupController.getRecords';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class LabourDataSearch extends LightningElement {}
+const DELAY = 300;
+export default class LabourDataSearch extends LightningElement {
+    @track accountNames;
+    @track isLoading=false;
+    @track selectedRecord;
+    delayTimeout;
+    objectLabel='Account';
+    ICON_URL = '/apexpages/slds/latest/assets/icons/{0}-sprite/svg/symbols.svg#{1}';
+
+    handleInputChange(event){
+        window.clearTimeout(this.delayTimeout);
+        const searchKey = event.target.value;
+        //this.isLoading = true;
+        this.delayTimeout = setTimeout(() => {
+            if(searchKey.length >= 2){
+                lookupSearchController({ 
+                    objName : 'Account',
+                    inputTxt : searchKey 
+                })
+                .then(result => {
+                     this.accountNames =result;
+                     console.log('Accounts'+JSON.stringify(this.accountNames));
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                })
+                .finally( ()=>{
+                    //this.isLoading = false;
+                });
+            }
+        }, DELAY);
+}
+
+handleSelect(event){
+        
+    let recordId = event.currentTarget.dataset.recordId;
+    
+    let selectRecord = this.accountNames.find((item) => {
+        return item.Id === recordId;
+    });
+    this.selectedRecord = selectRecord;
+    
+    const selectedEvent = new CustomEvent('lookup', {
+        bubbles    : true,
+        composed   : true,
+        cancelable : true,
+        detail: {  
+            data : {
+                record          : selectRecord,
+                recordId        : recordId,
+                currentRecordId : this.currentRecordId
+            }
+        }
+    });
+    this.dispatchEvent(selectedEvent);
+}
+
+handleClose(){
+    this.selectedRecord = undefined;
+    this.accountNames  = undefined;
+    const selectedEvent = new CustomEvent('lookup', {
+        bubbles    : true,
+        composed   : true,
+        cancelable : true,
+        detail: {  
+            record ,
+            recordId,
+            currentRecordId : this.currentRecordId
+        }
+    });
+    this.dispatchEvent(selectedEvent);
+}
+}
